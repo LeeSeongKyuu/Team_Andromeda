7fb806194ea28a8b82b4c37c9e200da03c9174f4
senajang <57481411+senajang@users.noreply.github.com>
2021-06-11 02:31:47 +0900

    rentSelect 추가 mypageUI

---
+++ UMB/app/src/main/java/good/ss/um/RentselectActivity.java
@@ -0,0 +1,51 @@
+package good.ss.um;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class RentselectActivity extends AppCompatActivity {
+
+    TextView
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+        //Initialize & assign variable
+
+        BottomNavigationView bottomNavigationView =findViewById(R.id.bottom_navigation);
+        //set home selected
+        bottomNavigationView.setSelectedItemId((R.id.mypage));
+
+        //perform itemselectedlistner
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.weather:
+                        startActivity(new Intent(getApplicationContext()
+                                , WeatherActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,MapsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.mypage:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
